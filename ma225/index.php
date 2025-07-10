11f5012238c1dfb7659a5e0737e8beb4da0bab7c
Douglas Speck <speck.empresarial@gmail.com>
2025-07-10 13:38:09 -0300

    MA225

---
+++ ma225/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>MA225 - Versão atual do trabalho</h1>
+    <p>Eu sou incapaz de ficar satisfeito com meu trabalho, então até que você corrija, <a target="_blank" href="versaoAtual.pdf">essa</a> é a versão mais atual.</p>
+</body>
+</html>
