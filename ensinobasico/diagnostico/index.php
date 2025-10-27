04a805b8d8bd565fb256b571afdd6999d52adf4c
Douglas Speck <speck.empresarial@gmail.com>
2025-10-27 09:00:25 -0300

    /ensinobasico

--- ensinobasico/diagnostico/index.php
+++ ensinobasico/diagnostico/index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://sites.google.com/unicamp.br/cadernosautocorretivosfracoes/diagn%C3%B3stico-em-aritm%C3%A9tica?authuser=0");
+header("Location: https://sites.google.com/unicamp.br/cadernosautocorretivosfracoes/diagn%C3%B3stico-de-aritm%C3%A9tica");
 exit();
 ?>
